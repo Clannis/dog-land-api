aa0df5e3ea9dd227370b9b58c3563942c24c4fe5
Clannis <dodds1990@gmail.com>
2021-02-04 01:10:10 -0600

    Bundle install. Generate and populate user model

---
+++ app/models/user.rb
@@ -0,0 +1,51 @@
+class User < ApplicationRecord
+    has_many :dogs
+    has_many :training_sessions, through: :dogs
+    has_many :courses, through: :training_sessions
+    has_secure_password
+
+    validates :username, uniqueness: true, on: :create
+    validates :email, uniqueness: true
+    validates :username, :first_name, :last_name, :phone_number, :email, presence: true
+    validates :password, presence: true, on: :create
+    validates :phone_number, numericality: { only_integer: true }
+    validates :password, confirmation: { case_sensitive: true}
+
+    def name
+        self.first_name.downcase.capitalize + " " + self.last_name.downcase.capitalize
+    end
+
+    def display_phone_number
+        "(#{self.phone_number[0..2]}) #{self.phone_number[3..5]} - #{self.phone_number[6..9]}"
+    end
+
+    def self.find_user(email)
+        user = where(email: email)
+        if user.count > 1
+            nil
+        else
+            user.first
+        end
+    end
+
+    def self.find_or_create_user_by_github(resource)
+        result = {created: false}
+        user = self.find_user(resource['email'].downcase)
+        if !user
+            name = resource['name'].split(" ")
+            user = self.create(
+                first_name: name[0], 
+                last_name: (name[1] ? name[1] : "Add Last Name"), 
+                username: resource['nickname'],
+                phone_number: 0000000000,
+                email: resource['email'].downcase,
+                password: "add_password",
+                password_confirmation: "add_password"
+            )
+            result[:created] = true
+        end
+        result[:user] = user
+        result
+    end
+
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    username { "MyString" }
+    password_digest { "MyString" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    phone_number { "MyString" }
+    email { "MyString" }
+  end
+end
