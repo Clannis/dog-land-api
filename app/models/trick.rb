bceba669704fa6545d9e7d39585b55bc67ca13fc
Clannis <dodds1990@gmail.com>
2021-02-04 03:47:19 -0600

    Copy over trick model from old app

--- app/models/trick.rb
+++ app/models/trick.rb
@@ -1,2 +1,29 @@
 class Trick < ApplicationRecord
+    has_many :trick_courses
+    has_many :courses, through: :trick_courses
+
+    validates :name, uniqueness: {case_sensitive: false}
+    validates :name, :description, :difficulty_rating, presence: true
+
+    def trick_course(course)
+        if self.trick_courses.count == 0
+            nil
+        else
+            result = nil
+            self.trick_courses.each do |trick_course|
+                if trick_course.course == course
+                    result = trick_course
+                end
+            end
+            result
+        end
+    end
+
+    def is_taught?
+        if self.courses.count > 0
+            true
+        else
+            false
+        end
+    end
 end
