8191002fe90977f5ed9a0ac95dc0a552a40694c8
Clannis <dodds1990@gmail.com>
2021-02-04 23:22:32 -0600

    Add session create method. Update user_params to use email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,17 @@
 class SessionsController < ApplicationController
     def create
-
+        user = User.find_by(email: user_params[:email])
+        if user && user.authenticate(user_params[:password])
+            token = encode_token(user.id)
+            render json: { user: UserSerializer.new(user), token: token }
+        else
+            render json: { errors: "Invalid Email or Password"} 
+        end
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:email, :password)
     end
 end
