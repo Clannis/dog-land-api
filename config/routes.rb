5f7cdfe7d49faec1fc49860be2ad7e4d28b77717
Clannis <dodds1990@gmail.com>
2021-02-04 19:18:22 -0600

    Copy over userss resources routes from old app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show, :index, :create, :edit, :update] do
+    resources :dogs, only: [:new, :create, :show, :index, :destroy]
+  end
+  
   post '/login', to: 'sessions#create'
 end
