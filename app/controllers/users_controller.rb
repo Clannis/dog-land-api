2b580befc6ad4a7e09ffa3b396506ade830f8235
Clannis <dodds1990@gmail.com>
2021-02-04 22:07:28 -0600

    Add dotenv, jwt, and active_model_serializers gems. Add JWT token methods and utilize them in new user create method. Update .gitignore. Generate user serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
 
     def create
+        user = User.new(user_params)
+        if user.save
+            token = encode_token(user.id)
+            render json: { user: UserSerializer.new(user), token: token }
+        else
+            render json: { errors: user.errors.full_messages.to_sentence} 
+        end
+    end
 
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :first_name, :last_name, :phone_number, :email, :password, :password_confirmation)
     end
 
 end
