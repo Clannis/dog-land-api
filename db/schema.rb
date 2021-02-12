04cf1cfa71d052d119c503e80124355687c2c5da
Clannis <dodds1990@gmail.com>
2021-02-11 23:22:00 -0600

    Add level column to courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_094009) do
+ActiveRecord::Schema.define(version: 2021_02_12_052032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_094009) do
     t.integer "cost"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "level"
   end
 
   create_table "dogs", force: :cascade do |t|
