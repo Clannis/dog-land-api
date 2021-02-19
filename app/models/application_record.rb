cf7b3735bf163bf343c9385d9f5e92b69848d413
Clannis <dodds1990@gmail.com>
2021-02-18 20:43:30 -0600

    Create set _default_avatar method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def set_default_avatar
+    unless self.avatar.attached?
+      self.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'dog_default.jpg')), filename: 'default-image.jpg', content_type: 'image/jpg')
+    end
+  end
+
 end
